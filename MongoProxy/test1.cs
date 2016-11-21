b1a58457c7f6db9930da3c11bb5989c0eb61e740
Liuzhangxi <liuzhangxi@yuezixixi.com>
2016-11-21 17:49:32 +0800

    Add files via upload

---
+++ MongoProxy/test1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using OUDAL.Properties;
+
+namespace OUDAL.MongoProxy
+{
+    public class MongoProxy
+    {
+        private static MongoClient client;
+
+        private static IMongoDatabase GetDB()
+        {
+            return client.GetDatabase(Resources.DatabaseName);
+        }
+
+        private static IMongoCollection<Campaign> GetCampaignCollection()
+        {
+            return client.GetDatabase(OUDAL.Properties.Resources.DatabaseName).GetCollection<Campaign>("Campaign");
+        }
+       
+        public static void Init()
+        {
+             //System.Runtime.InteropServices.RuntimeInformation
+            client = new MongoClient(OUDAL.Properties.Resources.MongoDB);
+        }
+
+        public static bool IsNotValidObjectId(string id)
+        {
+            ObjectId targetId;
+            return !ObjectId.TryParse(id, out targetId);
+        }
+
+        #region Campaign
+
+        public static Campaign InsertCampaign(Campaign campaign)
+        {
+            var cols = GetCampaignCollection();
+            cols.InsertOne(campaign);
+
+            return campaign;
+        }
+
+        public static UpdateResult UpdateCampaign(Campaign campaign)
+        {
+            var cols = GetCampaignCollection();
+            var filter = Builders<Campaign>.Filter.Eq(a => a.Id, campaign.Id);
+            var update = Builders<Campaign>.Update
+                .Set(a => a.Title, campaign.Title)
+                .Set(a => a.PageType, campaign.PageType)
+                .Set(a => a.ImageUrl, campaign.ImageUrl)
+                .Set(a => a.RedirectUrl, campaign.RedirectUrl)
+                .Set(a => a.Description, campaign.Description)
+                .Set(a => a.Seq, campaign.Seq)
+                .Set(a => a.StartTime, campaign.StartTime)
+                .Set(a => a.EndTime, campaign.EndTime)
+                .Set(a => a.CreateTime, campaign.CreateTime);
+
+            return cols.UpdateOne(filter, update);
+        }
+
+        public static DeleteResult DeleteCampaign(string campaignId)
+        {
+            var cols = GetCampaignCollection();
+            var filter = Builders<Campaign>.Filter.Eq(a => a.Id, ObjectId.Parse(campaignId));
+
+            return cols.DeleteOne(filter);
+        }
+
+        public static List<Campaign> GetCampaigns(DateTime? startTime, DateTime? endTime, int start, int rows, out int totalCount)
+        {
+            var cols = GetCampaignCollection();
+            var filter = Builders<Campaign>.Filter.Empty;
+            if (startTime.HasValue)
+            {
+                filter = filter & Builders<Campaign>.Filter.Lte(a => a.StartTime, startTime.Value);
+            }
+            if (endTime.HasValue)
+            {
+                filter = filter & Builders<Campaign>.Filter.Gte(a => a.EndTime, endTime.Value);
+            }
+            var cursor = cols.Find(filter);
+
+            totalCount = (int)cursor.Count();
+            var temp = cursor.Sort(Builders<Campaign>.Sort.Descending(a => a.CreateTime)).Skip(start).Limit(rows);
+
+            return temp.ToList();
+        }
+
+        public static Campaign GetCampaign(string id)
+        {
+            var cols = GetCampaignCollection();
+            var filter = Builders<Campaign>.Filter.Eq(a => a.Id, ObjectId.Parse(id));
+            var cursor = cols.Find(filter);
+
+            return cursor.FirstOrDefault();
+        }
+
+        #endregion
+         
+
+    }
+}
---
+++ MongoProxy/testModel1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using Newtonsoft.Json;
+
+namespace OUDAL.MongoProxy
+{
+    /// <summary>
+    /// 活动
+    /// </summary>
+    [BsonIgnoreExtraElements]
+    public class Campaign
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+
+        [BsonIgnore]
+        [JsonProperty(PropertyName = "Id")]
+        public string JsonId
+        {
+            get { return Id.ToString(); }
+            set { Id = ObjectId.Parse(value); }
+        }
+
+        /// <summary>
+        /// 广告标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// WebView类型, Default:简单展示, Article:文章详情
+        /// </summary>
+        public string PageType { get; set; }
+
+        /// <summary>
+        /// 广告图片
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// 导向链接,绝对路径
+        /// </summary>
+        public string RedirectUrl { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public int Seq { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime EndTime { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
